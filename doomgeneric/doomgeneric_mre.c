e9112351687ca30adedfd701cd7b672ccd8e2611
XimikBoda <46135530+XimikBoda@users.noreply.github.com>
2025-01-08 18:57:50 +0200

    Base realization, now it works only on MoDis

--- doomgeneric/doomgeneric_mre.c
+++ doomgeneric/doomgeneric_mre.c
@@ -5,19 +5,45 @@
 #include "vmstdlib.h"
 #include "vm4res.h"
 #include "vmres.h"
+#include "vmtimer.h"
+
+#include "doomgeneric.h"
 
 VMINT		layer_hdl[1];	// layer handle array. 
 VMUINT8* layer_buf = 0;
 
 VMINT screen_w = 0;
 VMINT screen_h = 0;
-VMUINT8* demo_canvas = 0;
 
 void handle_sysevt(VMINT message, VMINT param); // system events 
 void handle_keyevt(VMINT event, VMINT keycode); // key events 
-void handle_penevt(VMINT event, VMINT x, VMINT y); // pen events
 
-static void draw_hello(void);
+void DG_Init() {}
+void DG_DrawFrame() {
+	VMUINT16* layer_buf16 = (VMUINT16*)layer_buf;
+	for (int y = 0; y < DOOMGENERIC_RESY; ++y)
+		for (int x = 0; x < DOOMGENERIC_RESX; ++x) {
+			uint32_t c = DG_ScreenBuffer[y * DOOMGENERIC_RESX + x];
+			uint8_t* cc = (uint8_t*)&c;
+			layer_buf16[y + (DOOMGENERIC_RESX - x) * 240] = VM_COLOR_888_TO_565(cc[2], cc[1], cc[0]);
+		}
+	vm_graphic_flush_layer(layer_hdl, 1);
+}
+void DG_SleepMs(uint32_t ms) {}
+
+uint32_t DG_GetTicksMs() {
+	return vm_get_tick_count();
+}
+
+int DG_GetKey(int* pressed, unsigned char* key) {
+	return 0;
+}
+
+void DG_SetWindowTitle(const char* title) {}
+
+void timer(int tid) {
+	doomgeneric_Tick();
+}
 
 
 void vm_main(void) {
@@ -27,8 +53,15 @@ void vm_main(void) {
 	
 	//vm_reg_sysevt_callback(handle_sysevt);
 	vm_reg_keyboard_callback(handle_keyevt);
-	vm_reg_pen_callback(handle_penevt);
 
+	layer_hdl[0] = vm_graphic_create_layer(0, 0, screen_w, screen_h, -1);
+	layer_buf = vm_graphic_get_layer_buffer(layer_hdl[0]);
+	vm_graphic_set_clip(0, 0, screen_w, screen_h);
+
+	char* argv[3] = { 0, "-iwad", "C:\\DOOM.WAD" };
+	doomgeneric_Create(3, argv);
+
+	vm_create_timer(1, timer);
 }
 
 void handle_sysevt(VMINT message, VMINT param) {
@@ -43,7 +76,6 @@ void handle_sysevt(VMINT message, VMINT param) {
 		
 		vm_graphic_set_clip(0, 0, screen_w, screen_h);
 		
-		draw_hello();
 		break;
 	case VM_MSG_HIDE:	
 		if( layer_hdl[0] != -1 )
@@ -72,7 +104,6 @@ void handle_sysevt(VMINT message, VMINT param) {
 		break;
 		
 	case VM_MSG_PAINT:
-		draw_hello();
 		break;
 		
 	case VM_MSG_INACTIVE:
@@ -89,51 +120,4 @@ void handle_sysevt(VMINT message, VMINT param) {
 }
 
 void handle_keyevt(VMINT event, VMINT keycode) {
-	if( layer_hdl[0] != -1 ) {
-		vm_graphic_delete_layer(layer_hdl[0]);
-		layer_hdl[0] = -1;
-	}
-	
-	vm_exit_app();	
 }
-
-void handle_penevt(VMINT event, VMINT x, VMINT y) {
-	if( layer_hdl[0] != -1 ){
-		vm_graphic_delete_layer(layer_hdl[0]);
-		layer_hdl[0] = -1;
-	}
-	
-	vm_exit_app();
-}
-
-static void draw_hello(void) {
-
-	VMWCHAR s[100];
-	int x;
-	int y;
-	int wstr_len;
-	vm_graphic_color color;	
-
-	
-	vm_ascii_to_ucs2(s, 200, "Hello Cmake!");
-	
-	wstr_len = vm_graphic_get_string_width(s);
-
-	x = (screen_w - wstr_len) / 2;
-	y = (screen_h - vm_graphic_get_character_height()) / 2;
-	
-	color.vm_color_565 = VM_COLOR_WHITE;
-	vm_graphic_setcolor(&color);
-	
-	vm_graphic_fill_rect_ex(layer_hdl[0], 0, 0, screen_w, screen_h);
-	
-	color.vm_color_565 = VM_COLOR_BLUE;
-	vm_graphic_setcolor(&color);
-	
-	vm_graphic_textout_to_layer(layer_hdl[0],x, y, s, vm_wstrlen(s)); 
-
-	vm_graphic_blt(layer_buf, (screen_w - 100) / 2, screen_h - 100, demo_canvas, 0, 0, 100, 100, 1);
-	
-	vm_graphic_flush_layer(layer_hdl, 1);
-}
-
